c1f8bfa93b63d8612399c2d6914052d1aa04b5cc
Taisuke Miyazaki <imishinist@gmail.com>
2021-07-16 12:43:13 +0900

    size check

--- image.go
+++ image.go
@@ -30,6 +30,9 @@ func (h *HeatMapImage) Bounds() image.Rectangle {
 }
 
 func (h *HeatMapImage) At(x, y int) color.Color {
+	if len(h.heatmap.CountsInts2D[y]) < h.heatmap.Columns {
+		return color.RGBA{}
+	}
 	cnt := h.heatmap.CountsInts2D[y][x] & (1<<24 - 1)
 	r := (cnt >> 16)
 	g := (cnt >> 8)
